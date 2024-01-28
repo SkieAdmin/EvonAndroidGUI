ebfbf6874760d3bf338e0ae68e869151e9ee3d9e
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2024-01-28 21:51:12 +0800

    Update EvonRBLX.lua

--- EvonRBLX.lua
+++ EvonRBLX.lua
@@ -1,9 +1,11 @@
 --// Instances
 
+print("Matching The Player ID!")
 local test_mode = cloneref(game:GetService("Players"))
 if tostring(test_mode.LocalPlayer.UserId) == 4037923179 then
 	print("SkieHacker!")
 	warn("** Bypassed :TROLL:")
+	return;
 end
 
 local function EvonNotification(messages)
