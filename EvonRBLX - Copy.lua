57afa7a660c206908382db62c5130c6cf04292ac
SkieHacker <kerlemmorsti@gmail.com>
2025-04-04 05:56:49 +0800

    Revert Update

--- EvonRBLX - Copy.lua
+++ EvonRBLX - Copy.lua
@@ -23,7 +23,13 @@ else
 end
 
 if found then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Panda-Repositories/PandaKS_Libraries/main/EvonAPI/EvonLauncher.lua"))()
+	local new_ui = "https://raw.githubusercontent.com/Panda-Repositories/PandaKS_Libraries/refs/heads/main/EvonAPI/EvonBeta.lua"
+	local success, errorMessage = pcall(function()
+		loadstring(game:HttpGet(new_ui))()
+	end)
+	if not success then
+		warn("[Evon Error]" .. errorMessage)
+	end	
 else
 	-- Launch Regular (Evon)
 --// Instances
